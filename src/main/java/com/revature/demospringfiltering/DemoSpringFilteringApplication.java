7585394c546f590d4f87801fc20d216e4185e293
Kyle Plummer <kyle.plummer@revature.com>
2024-04-02 16:33:24 -0400

    initial

---
+++ src/main/java/com/revature/demospringfiltering/DemoSpringFilteringApplication.java
@@ -0,0 +1,13 @@
+package com.revature.demospringfiltering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringFilteringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringFilteringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/demospringfiltering/DemoSpringFilteringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.demospringfiltering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringFilteringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
